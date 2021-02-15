45a3979d630f486bf27394936068d7c0abf437cf
Erik Andrén Zachrisson <erik@zachrisson.info>
2021-02-15 15:33:59 +0100

    Somewhat working implementation

--- source/sntp.c
+++ source/sntp.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 
 #include <wiiuse/wpad.h>
+#include <ogc/lwp_queue.h>
 
 #include "ntp.h"
 #include "sysconf.h"
@@ -20,6 +21,33 @@ static GXRModeObj *rmode = NULL;
 
 static lwp_t ntp_handle = (lwp_t) NULL;
 
+#define MAX_QUEUE_ITEMS 4
+static lwp_queue queue;
+
+typedef struct _queue_item {
+	lwp_node node;
+	u32 buttonsDown;
+} queue_item;
+
+//static queue_item queue_items[MAX_QUEUE_ITEMS];
+
+static __inline__ lwp_node* __lwp_queue_head(lwp_queue *queue)
+{
+	return (lwp_node*)queue;
+}
+
+static __inline__ lwp_node* __lwp_queue_tail(lwp_queue *queue)
+{
+	return (lwp_node*)&queue->perm_null;
+}
+
+static __inline__ void __lwp_queue_init_empty(lwp_queue *queue)
+{
+	queue->first = __lwp_queue_tail(queue);
+	queue->perm_null = NULL;
+	queue->last = __lwp_queue_head(queue);
+}
+
 #define NO_RETRIES 20
 
 int main(int argc, char **argv) {
@@ -27,6 +55,8 @@ int main(int argc, char **argv) {
 	struct in_addr hostip;
 
 	xfb = initialise();
+
+	__lwp_queue_init_empty(&queue);
 
 	printf ("\nNTP client demo\n");
 	printf("Configuring network. Continously press home key to abort\n");
@@ -42,7 +72,7 @@ int main(int argc, char **argv) {
 	for (int i = 0; i < NO_RETRIES; i++) {
 		WPAD_ScanPads();
 
-		int buttonsDown = WPAD_ButtonsDown(0);
+	    u32 buttonsDown = WPAD_ButtonsDown(0);
 
 		if (buttonsDown & WPAD_BUTTON_HOME) {
 			printf("Home button pressed. Exiting...\n");
@@ -82,15 +112,31 @@ int main(int argc, char **argv) {
 		exit(0);
 	}
 
+	uint32_t buttonsDown;
 	while (true) {
 		VIDEO_WaitVSync();
 		WPAD_ScanPads();
 
-		int buttonsDown = WPAD_ButtonsDown(0);
+		buttonsDown = WPAD_ButtonsDown(0);
+		/* if (buttonsDown != 0) { */
+		/* 	printf("Buttons: 0x%0x\n", buttonsDown); */
+		/* } */
 
 		if (buttonsDown & WPAD_BUTTON_HOME) {
 			printf("Home button pressed. Exiting...\n");
 			exit(0);
+		}
+
+		if (buttonsDown != 0) {
+			queue_item *q = malloc(sizeof(queue_item));
+			if (q == NULL) {
+				printf("Failed to allocate queue item! Err:%d, %s\n", errno, strerror(errno));
+				continue;
+			}
+			q->buttonsDown = buttonsDown;
+
+//			printf("Enqueing queue with item: 0x%x\n", q->buttonsDown);
+			__lwp_queue_append(&queue, (lwp_node *) q);
 		}
 	}
 
@@ -105,7 +151,8 @@ void *ntp_client(void *arg) {
 	uint32_t rtc_s;
 	uint64_t local_time;
 	uint64_t ntp_time_in_gc_epoch;
-	u32 bias;
+	u32 bias, chk_bias;
+	s32 timezone;
 
 	memset(&packet, 0, sizeof(ntp_packet));
 
@@ -169,10 +216,12 @@ void *ntp_client(void *arg) {
 	packet.txTm_s = ntohl(packet.txTm_s);
 
 	n = __SYS_GetRTC(&rtc_s);
+	if (n == 0) {
+		printf("Failed to get RTC. Err: %d. Aborting!\n", n);
+		return NULL;
+	}
 
 	time_t txTm = (time_t) (packet.txTm_s - NTP_TO_UNIX_EPOCH_DELTA);
-	printf("Time: %s", ctime(&txTm));
-
 	ntp_time_in_gc_epoch = packet.txTm_s - NTP_TO_GC_EPOCH_DELTA;
 
 	n = SYSCONF_GetCounterBias(&bias);
@@ -185,31 +234,104 @@ void *ntp_client(void *arg) {
 	local_time = rtc_s + bias;
 	printf("ntp time: %llu, local time: %llu, diff: %lld\n", ntp_time_in_gc_epoch, local_time, ntp_time_in_gc_epoch - local_time);
 
+	printf("Use left and right button to adjust time zone of time below.\nPress A to write time to system memory\n");
+
+	printf("NTP Time: %s\n", ctime(&txTm));
+
+	timezone = 0;
+
+	// Calculate new bias
 	if (ntp_time_in_gc_epoch > local_time) {
-		// Need to increase counter bias to sync with ntp time
-		printf("Increasing bias by %llu\n", ntp_time_in_gc_epoch - local_time);
-		bias += (ntp_time_in_gc_epoch - local_time);
+		bias += ntp_time_in_gc_epoch - local_time;
 	} else {
-		printf("Decreasing bias by %llu\n", local_time - ntp_time_in_gc_epoch);
-		// Need to decrease counter bias to sync with ntp_time
-		bias -= (local_time - ntp_time_in_gc_epoch);
-	}
-	printf("Adjusting bias to %u\n", bias);
-	n = SYSCONF_SetCounterBias(bias);
-	if (n < 0) {
-		printf("Failed to reset counter bias. Err: %d. Aborting!\n", n);
-		return NULL;
+		bias -= local_time - ntp_time_in_gc_epoch;
 	}
 
-	printf("Rereading counter bias\n");
-	n = SYSCONF_GetCounterBias(&bias);
-	if (n < 0) {
-		printf("Failed to get counter bias. Err: %d. Aborting!\n", n);
-		return NULL;
+	while (true) {
+		uint32_t old_rtc_s = 0;
+
+		queue_item *q = (queue_item *) __lwp_queue_get(&queue);
+
+		n = __SYS_GetRTC(&rtc_s);
+		if (n == 0) {
+			printf("Failed to get RTC. Err: %d. Aborting!\n", n);
+			return NULL;
+		}
+
+		if (old_rtc_s != rtc_s) {
+			old_rtc_s = rtc_s;
+			local_time = rtc_s + bias + UNIX_EPOCH_TO_GC_EPOCH_DELTA;
+
+			char s[80];
+			struct tm * p = localtime((time_t *) &local_time);
+			strftime(s, 80, "%H:%M:%S %A %B %d %Y", p);
+
+			printf("\rNew system time: %s (Timezone: %+03d)", s, timezone);
+		}
+
+		if (q == NULL) {
+			//FIXME: Replace with thread yield
+			usleep(50 * 1000);
+			continue;
+		}
+
+		if (q->buttonsDown & WPAD_BUTTON_LEFT) {
+			if (timezone > -12) {
+				timezone--;
+				bias -= 3600;
+			}
+
+		} else if (q->buttonsDown & WPAD_BUTTON_RIGHT) {
+			if (timezone < 12) {
+				timezone++;
+				bias += 3600;
+			}
+
+		} else if (q->buttonsDown & WPAD_BUTTON_A) {
+			printf("\nWriting new time (bias) to sysconf\n");
+			n = SYSCONF_SetCounterBias(bias);
+			if (n < 0) {
+				printf("Failed to set counter bias. Err: %d. Aborting!\n", n);
+				return NULL;
+			}
+			printf("Checking written counter bias value\n");
+			chk_bias = 0;
+			n = SYSCONF_GetCounterBias(&chk_bias);
+			if (n < 0) {
+				printf("Failed to get counter bias. Err: %d. Aborting!\n", n);
+				return NULL;
+			}
+
+			if (bias != chk_bias) {
+				printf("Failed to verify written bias value. Got %u, expected %u\n", chk_bias, bias);
+				return NULL;
+			}
+
+			printf("Counter bias successfully updated. You can now terminate this program by pressing the home key or continue to alter the time zones\n");
+		}
+		free(q);
 	}
 
-	local_time = rtc_s + bias;
-	printf("ntp time: %llu, local time: %llu, diff: %lld\n", ntp_time_in_gc_epoch, local_time, ntp_time_in_gc_epoch - local_time);
+	/* if (ntp_time_in_gc_epoch > local_time) { */
+	/* 	// Need to increase counter bias to sync with ntp time */
+	/* 	printf("Increasing bias by %llu\n", ntp_time_in_gc_epoch - local_time); */
+	/* 	bias += (ntp_time_in_gc_epoch - local_time); */
+	/* } else { */
+	/* 	printf("Decreasing bias by %llu\n", local_time - ntp_time_in_gc_epoch); */
+	/* 	// Need to decrease counter bias to sync with ntp_time */
+	/* 	bias -= (local_time - ntp_time_in_gc_epoch); */
+	/* } */
+	/* printf("Adjusting bias to %u\n", bias); */
+
+	/* printf("Rereading counter bias\n"); */
+	/* n = SYSCONF_GetCounterBias(&bias); */
+	/* if (n < 0) { */
+	/* 	printf("Failed to get counter bias. Err: %d. Aborting!\n", n); */
+	/* 	return NULL; */
+	/* } */
+
+	/* local_time = rtc_s + bias; */
+	/* printf("ntp time: %llu, local time: %llu, diff: %lld\n", ntp_time_in_gc_epoch, local_time, ntp_time_in_gc_epoch - local_time); */
 
 
 	return NULL;
@@ -222,7 +344,9 @@ void *initialise() {
 	void *framebuffer;
 
 	VIDEO_Init();
-	WPAD_Init();
+	if (WPAD_Init() != WPAD_ERR_NONE) {
+		printf("Failed to initialize any wii motes\n");
+	}
 
 	rmode = VIDEO_GetPreferredMode(NULL);
 	framebuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
@@ -238,5 +362,4 @@ void *initialise() {
 	}
 
 	return framebuffer;
-
 }
